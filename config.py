15478bee53b5f1bdb7a594bb170cfef4fe6c4f5f
AnimeKaizoku <animekaizokux@gmail.com>
2020-07-14 14:55:06 +0530

    why...wont ignore work

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","awoo")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","uguu")
-    AUTHORIZED_USERS = [792109647,353689802,660565862]
-    TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","weeaboo")
